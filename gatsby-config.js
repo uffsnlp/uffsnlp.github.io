b8f687df3533a2571fbaf87a802c8adcefc59821
Junior V. Ramisch <junior.ramisch@gmail.com>
2021-02-23 15:38:48 -0300

    chore: adiciona o plugin gatsby-transformer-ipynb

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,28 @@ module.exports = {
     /* Your site config here */
     plugins: [
         "gatsby-plugin-sass",
-        
         {
             resolve: "gatsby-plugin-scroll-reveal",
             options: {
                 once: false,
             }
-        }
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "notebooks",
+                path: `${__dirname}/src/posts/notebooks`,
+                ignore: ["**/.ipynb_checkpoints"],
+            },
+        },
+        {
+            resolve: "@rafaelquintanilha/gatsby-transformer-ipynb",
+            options: {
+                notebookProps: {
+                    displayOrder: ["image/png", "text/html", "text/plain"],
+                    showPrompt: false,
+                },
+            },
+        },
     ],
 }
